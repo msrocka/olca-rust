37712181c8a48d469779e671f62d5b66fd91d32c
msrocka <michael.srocka@gmail.com>
2019-02-09 13:11:03 +0100

    wow, it seems to work on Windows now
    we just have to put the jumf.dll into the bin folder where
    the other libraries are located

--- java/jumf/src/main/java/jumf/Main.java
+++ java/jumf/src/main/java/jumf/Main.java
@@ -10,5 +10,22 @@ public class Main {
 		System.out.println("load " + file);
 		System.load(file.getAbsolutePath());
 		System.out.println(FFI.sum(new double[] { 1, 2, 3 }));
+
+		double[] x = new double[5];
+		FFI.solve(5,
+				new int[] { 0, 2, 5, 9, 10, 12 },
+				new int[] { 0, 1, 0, 2, 4, 1, 2, 3, 4, 2, 1, 4 },
+				new double[] { 2., 3., 3., -1., 4., 4., -3., 1., 2., 2., 6.,
+						1. },
+				new double[] { 8., 45., -3., 3., 19. },
+				x);
+
+		// assertArrayEquals(
+		//		new double[] { 1d, 2d, 3d, 4d, 5d }, x, 1e-8);
+
+		for (int i = 0; i < x.length; i++) {
+			System.out.println("x["+ i +"] = " + x[i]);
+		}
+
 	}
 }
