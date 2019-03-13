cc4ca29c17564b965a92231741cfc2c67e69b117
msrocka <michael.srocka@gmail.com>
2019-03-13 15:23:03 +0100

    added UMFPACK tests

--- java/src/test/java/olcar/UmfpackTest.java
+++ java/src/test/java/olcar/UmfpackTest.java
@@ -1,13 +1,17 @@
 package olcar;
 
 import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assume.assumeTrue;
 
 import java.io.File;
 
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
+import org.openlca.core.matrix.format.CCRMatrix;
+import org.openlca.core.matrix.format.HashPointMatrix;
 import org.openlca.julia.Julia;
+import org.openlca.julia.UmfFactorizedMatrix;
+import org.openlca.julia.Umfpack;
 
 public class UmfpackTest {
 
@@ -17,8 +21,7 @@ public class UmfpackTest {
 	}
 
 	@Test
-	public void testSolve() {
-		assumeTrue("library with UMFPACK support", FFI.isWithUmfpack());
+	public void testSolveNative() {
 		double[] x = new double[5];
 		Julia.umfSolve(5,
 				new int[] { 0, 2, 5, 9, 10, 12 },
@@ -27,7 +30,41 @@ public class UmfpackTest {
 						1. },
 				new double[] { 8., 45., -3., 3., 19. },
 				x);
-		 assertArrayEquals(
+		assertArrayEquals(
 				new double[] { 1d, 2d, 3d, 4d, 5d }, x, 1e-8);
+	}
+
+	@Test
+	public void testSolveMatrix() {
+		HashPointMatrix m = new HashPointMatrix(new double[][] {
+				{ 2.0, 3.0, 0.0, 0.0, 0.0 },
+				{ 3.0, 0.0, 4.0, 0.0, 6.0 },
+				{ 0.0, -1.0, -3.0, 2.0, 0.0 },
+				{ 0.0, 0.0, 1.0, 0.0, 0.0 },
+				{ 0.0, 4.0, 2.0, 0.0, 1.0 } });
+		CCRMatrix uMatrix = CCRMatrix.of(m);
+		double[] demand = { 8., 45., -3., 3., 19. };
+		double[] x = Umfpack.solve(uMatrix, demand);
+		assertArrayEquals(
+				new double[] { 1d, 2d, 3d, 4d, 5d }, x, 1e-8);
+	}
+
+	@Test
+	@Ignore
+	public void testFactorizeMatrix() {
+		HashPointMatrix m = new HashPointMatrix(new double[][] {
+				{ 2.0, 3.0, 0.0, 0.0, 0.0 },
+				{ 3.0, 0.0, 4.0, 0.0, 6.0 },
+				{ 0.0, -1.0, -3.0, 2.0, 0.0 },
+				{ 0.0, 0.0, 1.0, 0.0, 0.0 },
+				{ 0.0, 4.0, 2.0, 0.0, 1.0 } });
+		CCRMatrix uMatrix = CCRMatrix.of(m);
+		UmfFactorizedMatrix factorizedM = Umfpack.factorize(uMatrix);
+
+		double[] demand = { 8., 45., -3., 3., 19. };
+		double[] x = Umfpack.solve(factorizedM, demand);
+		assertArrayEquals(
+				new double[] { 1d, 2d, 3d, 4d, 5d }, x, 1e-8);
+		factorizedM.dispose();
 	}
 }
