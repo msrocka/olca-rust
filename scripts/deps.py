53348f01b8034037d96349fa352b5d09c933ed67
msrocka <michael.srocka@gmail.com>
2019-09-30 09:04:39 +0200

    calculate the dependency DAG
    digraph g {
      "libolcar_withumf.dylib" -> "libumfpack.5.7.8.dylib";
      "libolcar_withumf.dylib" -> "libopenblas64_.0.3.5.dylib";
      "libumfpack.5.7.8.dylib" -> "libamd.2.4.6.dylib";
      "libumfpack.5.7.8.dylib" -> "libsuitesparseconfig.5.4.0.dylib";
      "libumfpack.5.7.8.dylib" -> "libopenblas64_.0.3.5.dylib";
      "libumfpack.5.7.8.dylib" -> "libcholmod.3.0.13.dylib";
      "libopenblas64_.0.3.5.dylib" -> "libgfortran.5.dylib";
      "libopenblas64_.0.3.5.dylib" -> "libquadmath.0.dylib";
      "libopenblas64_.0.3.5.dylib" -> "libgcc_s.1.dylib";
      "libamd.2.4.6.dylib" -> "libsuitesparseconfig.5.4.0.dylib";
      "libcholmod.3.0.13.dylib" -> "libamd.2.4.6.dylib";
      "libcholmod.3.0.13.dylib" -> "libcolamd.2.9.6.dylib";
      "libcholmod.3.0.13.dylib" -> "libsuitesparseconfig.5.4.0.dylib";
      "libcholmod.3.0.13.dylib" -> "libccolamd.2.9.6.dylib";
      "libcholmod.3.0.13.dylib" -> "libcamd.2.4.6.dylib";
      "libcholmod.3.0.13.dylib" -> "libopenblas64_.0.3.5.dylib";
      "libgfortran.5.dylib" -> "libquadmath.0.dylib";
      "libgfortran.5.dylib" -> "libgcc_s.1.dylib";
      "libquadmath.0.dylib" -> "libgcc_s.1.dylib";
      "libcolamd.2.9.6.dylib" -> "libsuitesparseconfig.5.4.0.dylib";
      "libccolamd.2.9.6.dylib" -> "libsuitesparseconfig.5.4.0.dylib";
      "libcamd.2.4.6.dylib" -> "libsuitesparseconfig.5.4.0.dylib";
    }

--- scripts/deps.py
+++ scripts/deps.py
@@ -1,16 +1,16 @@
 import os
-import os.path as path
 import platform
 import subprocess
 import sys
 
-PROJECT_ROOT = path.dirname(path.dirname(path.abspath(__file__)))
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 class Node:
 
-    def __init__(self, lib: str):
-        self.lib = lib
+    def __init__(self, path: str, name: str):
+        self.path = path
+        self.name = name
         self.deps = []
 
 
@@ -23,7 +23,7 @@ def get_julia_libdir():
         sys.exit("unknown platform: " + ps)
 
     libdir = None
-    config = path.join(PROJECT_ROOT, "config")
+    config = os.path.join(PROJECT_ROOT, "config")
     with open(config, "r", encoding="utf-8") as f:
         libdir_key = os_prefix + "-julia-lib-dir"
         for line in f.readlines():
@@ -52,9 +52,40 @@ def get_deps(lib_file: str, libs: list) -> list:
     deps = []
     for line in out.splitlines():
         for lib in libs:
+            if lib in lib_file:
+                continue
             if lib in line:
                 deps.append(lib)
     return deps
+
+
+def get_dep_dag(entry: str) -> Node:
+    libdir = get_julia_libdir()
+    libs = os.listdir(libdir)
+    handled = set()
+    root = Node(entry, entry.split(os.path.sep)[-1])
+    queue = [root]
+    while len(queue) != 0:
+        n = queue.pop(0)  # type: Node
+        for dep in get_deps(n.path, libs):
+            dep_node = Node(os.path.join(libdir, dep), dep)
+            n.deps.append(dep_node)
+            if dep in handled:
+                continue
+            handled.add(dep)
+            queue.append(dep_node)
+    return root
+
+
+def viz(dep_dag: Node):
+    print("digraph g {")
+    queue = [dep_dag]
+    while len(queue) != 0:
+        n = queue.pop(0)
+        for dep in n.deps:
+            print('  "%s" -> "%s";' % (n.name, dep.name))
+            queue.append(dep)
+    print("}")
 
 
 if __name__ == '__main__':
@@ -62,5 +93,7 @@ if __name__ == '__main__':
     if julia_libdir is None:
         sys.exit("Could not find the Julia lib folder")
     libs = os.listdir(julia_libdir)
-    entry = path.join(PROJECT_ROOT, "bin", "libolcar.dylib")
-    print(get_deps(entry, libs))
+    entry = os.path.join(PROJECT_ROOT, "bin", "libolcar_withumf.dylib")
+    # print(get_deps(entry, libs))
+    dag = get_dep_dag(entry)
+    viz(dag)
